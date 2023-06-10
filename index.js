0bb4679d9536a90b3cfd2fe14f067376771b2e00
Yuti Thakkar <thakkaryuti@gmail.com>
2023-06-10 13:58:18 -0400

    adds static method

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-class Formatter {
+
   //add static methods here
-}
+  class Formatter {
+    static capitalize(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    static sanitize(string) {
+      return string.replace(/[^a-z0-9\-'\s]/gi, '');
+    }
+
+    static titleize(string) {
+      const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+      const words = string.split(' ');
+
+      const titleizedWords = words.map((word, index) => {
+        if (index === 0 || !exceptions.includes(word)) {
+          return Formatter.capitalize(word);
+        } else {
+          return word;
+        }
+      });
+
+      return titleizedWords.join(' ');
+    }
+  }
+
